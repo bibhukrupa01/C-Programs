b36a3fa2c858094801ddfd53edac342b4e6a97e9
Bibhu krupa <bibhukrupa5@gmail.com>
2025-08-02 20:48:27 +0530

    Add files via upload

--- quickSort.c
+++ quickSort.c
@@ -1,48 +1,108 @@
-// QuickSort algorithm in C
+// // QuickSort algorithm in C
+// #include <stdio.h>
+
+// void quickSort(int arr[], int first, int last){
+//     int temp;
+//     int pivot = arr[first]; // First element as pivot
+//     if (first < last){
+//         int i = first;
+//         int j = last;
+
+//         while (i < j){
+//             while (arr[i] <= pivot){
+//                 i++;
+//             }
+//             while (arr[j] > pivot){
+//                 j--;
+//             }
+//             if (i < j){
+//                 temp = arr[i]; // swapping elements
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//         temp = arr[first]; //swapping with pivot when i is greater than j
+//         arr[first] = arr[j];
+//         arr[j] = temp;
+//         quickSort(arr, first, j-1);
+//         quickSort(arr, j+1, last);
+//     }
+// }
+
+// int main(){
+//     int n;
+//     printf("Enter the number of elements: ");
+//     scanf("%d", &n);
+//     int arr[n];
+//     printf("Enter the elements of the array:\n");
+//     for(int i = 0; i < n; i++){
+//         printf("arr[%d]: ",i);
+//         scanf("%d", &arr[i]);
+//     }
+//     quickSort(arr, 0, n-1);
+//     printf("Sorted array is:\n");
+//     for(int i = 0; i < n; i++){
+//         printf("%d\t", arr[i]);
+//     }
+//     return 0;
+// }
+
+
+//Quicksort (taking last element as pivot)
+
 #include <stdio.h>
+#include <stdlib.h>
 
-void quickSort(int arr[], int first, int last){
-    int temp;
-    int pivot = arr[first]; // First element as pivot
-    if (first < last){
-        int i = first;
-        int j = last;
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-        while (i < j){
-            while (arr[i] <= pivot){
-                i++;
-            }
-            while (arr[j] > pivot){
-                j--;
-            }
-            if (i < j){
-                temp = arr[i]; // swapping elements
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+int Partition(int A[], int p, int r)
+{
+    int x = A[r];
+    int i = p - 1;
+    for (int j = p; j < r; j++)
+    {
+        if (A[j] <= x)
+        {
+            i++;
+            swap(&A[i], &A[j]);
         }
-        temp = arr[first]; //swapping with pivot when i is greater than j
-        arr[first] = arr[j];
-        arr[j] = temp;
-        quickSort(arr, first, j-1);
-        quickSort(arr, j+1, last);
+    }
+
+    swap(&A[i + 1], &A[r]);
+    return i + 1;
+}
+
+void QuickSort(int A[], int p, int r)
+{
+    if (p < r)
+    {
+        int q = Partition(A, p, r);
+        QuickSort(A, p, q-1);
+        QuickSort(A, q+1, r);
     }
 }
 
-int main(){
+int main()
+{
     int n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
-    int arr[n];
-    printf("Enter the elements of the array:\n");
-    for(int i = 0; i < n; i++){
-        printf("arr[%d]: ",i);
-        scanf("%d", &arr[i]);
+    int A[n];
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &A[i]);
     }
-    quickSort(arr, 0, n-1);
-    printf("Sorted array is:\n");
-    for(int i = 0; i < n; i++){
-        printf("%d\t", arr[i]);
+    
+    QuickSort(A, 0, n - 1);
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
     }
-    return 0;
 }
