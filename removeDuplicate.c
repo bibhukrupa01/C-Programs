38d21280aa7fa207d45b46d3ee81d1cea4fb7310
Bibhu krupa <bibhukrupa5@gmail.com>
2025-08-24 22:24:18 +0530

    Create removeDuplicate.c

---
+++ removeDuplicate.c
@@ -0,0 +1,44 @@
+// Remove duplicate using singly linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+// Helper function to create a new node
+struct ListNode* createNode(int val) {
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->val = val;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    // Dummy head node
+    struct ListNode temp;
+    temp.next = NULL;
+    struct ListNode* tail = &temp;
+
+    while (list1 && list2) {
+        if (list1->val < list2->val) {
+            tail->next = list1;
+            list1 = list1->next;
+        } else {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (list1) {
+        tail->next = list1;
+    } else {
+        tail->next = list2;
+    }
+
+    return temp.next;
+}
